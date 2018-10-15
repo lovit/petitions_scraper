8000e50c95df9bf56ff88927e1ca0472c418ac7b
lovit <soy.lovit@gmail.com>
2018-10-15 19:44:39 +0900

    Commit show_categories

--- petitions_scrapper/__init__.py
+++ petitions_scrapper/__init__.py
@@ -1,2 +1,3 @@
 from .parser import parse_page
 from .utils import get_soup
+from .utils import show_categories
--- petitions_scrapper/utils.py
+++ petitions_scrapper/utils.py
@@ -13,6 +13,14 @@ def get_soup(url):
     page = BeautifulSoup(html, 'lxml')
     return page
 
+def show_categories():
+    url = 'https://www1.president.go.kr/petitions/category'
+    soup = get_soup(url)
+    for li in soup.select('div[class=category_area] li'):
+        name = li.text
+        category_idx = str(li).split('="')[-1].split('"')[0]
+        print('idx = {:3}, name = {}'.format(category_idx, name))
+
 doublespace_pattern = re.compile('\s+')
 lineseparator_pattern = re.compile('\n+')
 
