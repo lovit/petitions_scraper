3a1ef3b26929057319f324a2961d50bbed033eee
lovit <soy.lovit@gmail.com>
2018-11-07 00:06:42 +0900

    Add LongPetition (including content)

--- scripts/create_short_petition_table_from_json.py
+++ scripts/create_short_petition_table_from_json.py
@@ -15,6 +15,13 @@ class ShortPetition(
             return '\t'*6
         return strf
 
+class LongPetition(
+    collections.namedtuple('LongPetition',
+        'idx status begin end category num_agree title content')):
+
+    def __str__(self):
+        return '\t'.join([str(v) for v in self])
+
 def get_value(obj, key):
     value = obj[key]
     if isinstance(value, str):
@@ -29,13 +36,16 @@ def main():
     parser.add_argument('--json_directory', type=str, default='../output/')
     parser.add_argument('--table_path', type=str, default='./petitions.tsv')
     parser.add_argument('--debug', dest='debug', action='store_true')
+    parser.add_argument('--include_content', dest='long', action='store_true')
 
     args = parser.parse_args()
     json_directory = args.json_directory
     table_path = args.table_path
     debug = args.debug
+    long = args.long
 
-    keys = 'petition_idx status begin end category num_agree title'.split()
+    keys_short = 'petition_idx status begin end category num_agree title'.split()
+    keys_long = 'petition_idx status begin end category num_agree title content'.split()
 
     # get json paths
     json_paths = glob.glob('{}/*.json'.format(json_directory))
@@ -47,7 +57,10 @@ def main():
     for path in json_paths:
         with open(path, encoding='utf-8') as f:
             json_obj = json.load(f)
-        petition = ShortPetition(*get_values(json_obj, keys))
+        if long:
+            petition = LongPetition(*get_values(json_obj, keys_long))
+        else:
+            petition = ShortPetition(*get_values(json_obj, keys_short))
         rows.append(petition)
     rows = sorted(rows, key=lambda x:int(x.petition_idx))
 
